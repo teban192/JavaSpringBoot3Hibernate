b0d60f15ff9ee4ff37f1cc13f413423794fa68b0
teban192 <mmonestelalf@gmail.com>
2024-08-16 15:09:36 -0600

    config starters

--- mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,13 +1,16 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+    @Value("${coach.name}")
+    private String coachName;
     // expose "/" that return "Hello World"
     @GetMapping("/")
     public String sayHello(){
-        return "Hello Word";
+        return coachName;
     }
 }
