658d86db53a661cc3c5f2744a7ea42c3d05a794d
teban192 <mmonestelalf@gmail.com>
2024-09-02 09:25:46 -0600

    making progress

--- mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,16 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+    // inject properties for:coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
     // expose a "/" that returns "hello word"
     @GetMapping("/")
     public String saidHello(){
@@ -18,4 +24,10 @@ public class FunRestController {
     public String getDailyFortune(){
         return "Today is your lucky day!";
     }
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach"+ coachName +", Team name: " + teamName;
+    }
+
 }
