db3b0dbd3cabd18f09aa354fffdf97c1458c5e8c
teban192 <mmonestelalf@gmail.com>
2024-09-02 08:45:02 -0600

    Springboot course

--- mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,16 +1,21 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
-    @Value("${coach.name}")
-    private String coachName;
-    // expose "/" that return "Hello World"
+    // expose a "/" that returns "hello word"
     @GetMapping("/")
-    public String sayHello(){
-        return coachName;
+    public String saidHello(){
+        return "Hello world";
+    }
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run a hard 5k!";
+    }
+    @GetMapping("/fortune")
+    public String getDailyFortune(){
+        return "Today is your lucky day!";
     }
 }
