fcee0344d46206dfb68a46e738cb5a37a1e7680d
teban192 <mmonestelalf@gmail.com>
2024-09-08 12:52:01 -0600

    Spring Scope Singleton and Prototype

--- springcoredemo/springcoredemo/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ springcoredemo/springcoredemo/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     //define a private field for the dependency
     private Coach myCoach;
+    private Coach anotherCoach;
     //define a constructor for dependency injection
     @Autowired
 //    Setter injection
@@ -17,9 +18,10 @@ public class DemoController {
 //        myCoach = theCoach;
 //    }
 //    Constructor Injection
-    public DemoController(@Qualifier ("cricketCoach")Coach theCoach){
+    public DemoController(@Qualifier ("cricketCoach")Coach theCoach,@Qualifier("cricketCoach")Coach theAnotherCoach){
         System.out.println("In Constructor"+ getClass().getName());
         myCoach = theCoach;
+        anotherCoach=theAnotherCoach;
     }
     //using with primary
 //    public DemoController(Coach theCoach){
@@ -31,6 +33,11 @@ public class DemoController {
     @GetMapping("/dailyworkout")
     public String getDailyWorkout(){
         return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check(){
+        return "Comparing beans: myCoach== anotherCoach " +(myCoach==anotherCoach);
     }
 
 }
